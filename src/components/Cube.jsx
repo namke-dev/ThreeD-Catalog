58419e67f48a7eee899fce85fda51df4ee8c5a3d
namke-dev <namke.dev@gmail.com>
2024-01-24 03:36:50 +0700

    rename file

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,12 +1,12 @@
-import { Float, Shadow } from "@react-three/drei";
+import { Float } from "@react-three/drei";
 import React from "react";
 
-export default function Experience() {
+export default function Cube() {
   return (
     <Float rotationIntensity={2} floatIntensity={1} speed={2}>
       <mesh position={[0, 0, 0]} castShadow shadows>
         <boxGeometry args={[5, 5, 5]} />
-        <meshNormalMaterial />
+        <meshPhysicalMaterial color="orange" />
       </mesh>
     </Float>
   );
