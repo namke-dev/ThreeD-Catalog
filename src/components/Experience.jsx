a06813ff1df4aa30fe6d0fdf5ef709ffd2c30d85
namke-dev <namke.dev@gmail.com>
2024-01-24 02:44:51 +0700

    update foreground

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,13 @@
+import { Float, Shadow } from "@react-three/drei";
 import React from "react";
 
 export default function Experience() {
   return (
-    <mesh>
-      <boxGeometry />
-      <meshNormalMaterial />
-    </mesh>
+    <Float rotationIntensity={2} floatIntensity={10} speed={2}>
+      <mesh position={[0, 0, 0]} castShadow shadows>
+        <boxGeometry args={[6, 6, 6]} />
+        <meshNormalMaterial />
+      </mesh>
+    </Float>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,47 @@
 import Experience from "@/components/Experience";
+import {
+  AccumulativeShadows,
+  CameraControls,
+  RandomizedLight,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
 export default function App() {
   return (
     <div className="App h-[100vh] w-[100vw]">
-      <Canvas>
+      <Canvas
+        shadows
+        camera={{ position: [30, 0, -3], fov: 35, near: 1, far: 50 }}
+      >
+        <color attach="background" args={["#c6e5db"]} />
+        <CameraControls
+          truckSpeed={0}
+          dollySpeed={0}
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 2}
+        />
+
         <Experience />
+        {/** Soft shadows */}
+        <AccumulativeShadows
+          temporal
+          frames={100}
+          color="lightblue"
+          colorBlend={2}
+          opacity={0.7}
+          scale={60}
+          position={[0, -5, 0]}
+        >
+          <RandomizedLight
+            amount={4}
+            radius={15}
+            ambient={0.5}
+            intensity={4}
+            position={[-5, 10, -5]}
+            size={20}
+          />
+        </AccumulativeShadows>
       </Canvas>
     </div>
   );
