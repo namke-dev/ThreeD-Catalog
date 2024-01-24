56da8be0b59613045e420aca940bbc30ab548a51
namke-dev <namke.dev@gmail.com>
2024-01-25 03:29:24 +0700

    add sofa model texture

--- src/components/Sofa2.jsx
+++ src/components/Sofa2.jsx
@@ -1,26 +1,23 @@
 import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, useTexture } from "@react-three/drei";
 
 export function Sofa2(props) {
   const { nodes, materials } = useGLTF("/models/sofa2/scene.gltf");
+  const textureProps = useTexture({
+    map: "/models/sofa2/textures/ESTRUTUR_diffuse.jpeg",
+  });
   return (
     <group {...props} dispose={null}>
       <group
         position={[0.5, -0.5, 1.3]}
         rotation={[-Math.PI / 2, 0, Math.PI / 2]}
       >
-        <mesh
-          castShadow
-          shadows
-          geometry={nodes.Object_4.geometry}
-          material={materials.ESTRUTUR}
-        />
-        <mesh
-          castShadow
-          shadows
-          geometry={nodes.Object_5.geometry}
-          material={materials.ESTRUTUR}
-        />
+        <mesh castShadow shadows geometry={nodes.Object_4.geometry}>
+          <meshStandardMaterial {...textureProps} />
+        </mesh>
+        <mesh castShadow shadows geometry={nodes.Object_5.geometry}>
+          <meshStandardMaterial {...textureProps} />
+        </mesh>
       </group>
       {/* <group>
         <mesh castShadow shadows>
