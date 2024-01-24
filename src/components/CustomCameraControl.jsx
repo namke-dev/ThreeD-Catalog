a50ea32ab41acf4a8f03ab408d58d4dc1450a0cc
namke-dev <namke.dev@gmail.com>
2024-01-24 23:15:13 +0700

    Add custom camera controls

---
+++ src/components/CustomCameraControl.jsx
@@ -0,0 +1,21 @@
+import { OrbitControls } from "@react-three/drei";
+import React from "react";
+
+export default function CustomCameraControls() {
+  const minDistance = 4.5;
+  const maxDistance = 8;
+  return (
+    <OrbitControls
+      makeDefault
+      enablePan={false}
+      enableZoom={true}
+      minDistance={minDistance}
+      maxDistance={maxDistance}
+      minPolarAngle={Math.PI / 4}
+      maxPolarAngle={Math.PI / 2.3}
+      minAzimuthAngle={Math.PI / 6}
+      maxAzimuthAngle={Math.PI / 1.3}
+      rotateSpeed={0.3}
+    />
+  );
+}
--- src/components/ModelContainer.jsx
+++ src/components/ModelContainer.jsx
@@ -2,13 +2,14 @@ import { CameraControls, Environment, OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Suspense, useRef } from "react";
 import { CustomBackdrop } from "./Backdrop";
+import CustomCameraControls from "./CustomCameraControl";
 
 export default function ModelContainer({ children }) {
   return (
     <Canvas
       shadows
       camera={{
-        position: [5, 0, 0],
+        position: [6, 0, 0],
         fov: 35,
         near: 3,
         far: 10,
@@ -18,16 +19,7 @@ export default function ModelContainer({ children }) {
       <Environment preset="city" />
       {/* <color attach="background" args={["#c6e5db"]} /> */}
       <color attach="background" args={["#eeeeee"]} />
-      <OrbitControls
-        enablePan={false}
-        enableZoom={flase}
-        makeDefault
-        minPolarAngle={Math.PI / 4}
-        maxPolarAngle={Math.PI / 2.3}
-        minAzimuthAngle={Math.PI / 6}
-        maxAzimuthAngle={Math.PI / 1.3}
-        rotateSpeed={0.3}
-      />
+      <CustomCameraControls />
       <CustomBackdrop />
       <Suspense fallback={null}>{children}</Suspense>
     </Canvas>
