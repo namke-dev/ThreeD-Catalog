09bfdd5efc254aa04685bf31ad237b25d1e03a70
namke-dev <namke.dev@gmail.com>
2024-01-24 22:03:11 +0700

    fix layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,15 @@ import { Sofa2 } from "@/components/Sofa2";
 export default function App() {
   return (
     <div className="App bg-[#ffffff] flex flex-col">
-      <div className="flex flex-grow h-1/4 w-auto flex-1 bg-red-100"></div>
-      <div className="h-3/4 flex flex-row">
-        <div className="w-2/3">
+      <div className="flex flex-grow h-[10vh] w-auto flex-1 bg-red-100"></div>
+      <div className="h-[90vh] flex flex-row">
+        <div className="w-full">
           <ModelContainer>
-            {/* <Cube /> */}
-            {/* <DeskModel /> */}
-            <Sofa2 />
+            <DeskModel />
+            {/* <Sofa2 /> */}
           </ModelContainer>
         </div>
-        <div className="w-1/3 h-full bg-blue-200"></div>
+        {/* <div className="w-1/3 h-full bg-blue-200"></div> */}
       </div>
     </div>
   );
