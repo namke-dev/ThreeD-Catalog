aa2f31ac6cdc71ea8039bae10cd86aee62980fca
namke-dev <namke.dev@gmail.com>
2024-01-26 01:25:06 +0700

    format code

--- src/components/ModelContainer.jsx
+++ src/components/ModelContainer.jsx
@@ -4,6 +4,7 @@ import { CustomBackdrop } from "./Backdrop";
 import CustomCameraControls from "./CustomCameraControl";
 import {
   ambient_light,
+  ambient_light_intensity,
   page_background,
   room_area,
 } from "@/helpers/light-color-helper";
@@ -16,11 +17,11 @@ export default function ModelContainer({ children }) {
         position: [5, 1.5, 0],
         fov: 35,
         near: 3,
-        far: (room_area - 4.5) * 1.42,
+        far: room_area * 1.42,
       }}
     >
       <color attach="background" args={[page_background]} />
-      <ambientLight intensity={0.2} color={ambient_light} />
+      <ambientLight intensity={ambient_light_intensity} color={ambient_light} />
       <Environment preset="city" intensity={0} />
       <CustomCameraControls />
       <CustomBackdrop />
--- src/helpers/light-color-helper.js
+++ src/helpers/light-color-helper.js
@@ -1,6 +1,6 @@
 export const room_area = 15;
 export const ambient_light = "green";
-export const ambient_light_intensity = 1;
+export const ambient_light_intensity = 1.5;
 export const Backdrop_light_intensity = 1;
 
 export const room_wall_color = "#ffffff";
