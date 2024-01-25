b52ac47792b3b00f4d05251d4f137cfd60b20cd3
namke-dev <namke.dev@gmail.com>
2024-01-26 04:15:35 +0700

    remove azimuth limit

--- src/components/models/CustomCameraControl.jsx
+++ src/components/models/CustomCameraControl.jsx
@@ -14,9 +14,9 @@ export default function CustomCameraControls() {
       maxDistance={maxDistance}
       minPolarAngle={Math.PI / 4}
       maxPolarAngle={Math.PI / 2.32}
-      minAzimuthAngle={Math.PI / 6}
-      maxAzimuthAngle={Math.PI / 1.3}
       rotateSpeed={0.3}
     />
   );
+  // minAzimuthAngle={Math.PI / 6}
+  //     maxAzimuthAngle={Math.PI / 1.3}
 }
