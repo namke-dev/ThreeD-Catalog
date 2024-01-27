7fad09958706b98059d866f7758536abcb44e3d5
namke-dev <namke.dev@gmail.com>
2024-01-27 08:29:09 +0700

    format code

--- src/components/layouts/Cart.jsx
+++ src/components/layouts/Cart.jsx
@@ -8,9 +8,7 @@ export default function Cart({ className, children, ...props }) {
       bg-white/50
       text-black/70
       hover:bg-white/95
-      
-      p-0 m-0
-      
+      md:hover:scale-105
       rounded-sm
       
       transition-all
