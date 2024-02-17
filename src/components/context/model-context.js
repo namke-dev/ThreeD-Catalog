0fd05571d7671522ee7307bf81d1bd570e57be22
namke-dev <namke.dev@gmail.com>
2024-02-18 04:44:09 +0700

    Create DisplayModelContext

---
+++ src/components/context/model-context.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const DisplayModelContext = React.createContext();
+export default DisplayModelContext;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,19 @@
+import DisplayModelContext from "@/components/context/model-context";
+import { LIST_FURNITURE } from "@/data/product_custom_options";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { useState } from "react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const [displayModel, setDisplayModel] = useState(LIST_FURNITURE[0]);
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <DisplayModelContext.Provider value={{ displayModel, setDisplayModel }}>
+        <Component {...pageProps} />
+      </DisplayModelContext.Provider>
     </SessionProvider>
   );
 }
