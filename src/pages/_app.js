88c412da9bbca4c15ed78061d7c61541040ed95e
namke-dev <namke.dev@gmail.com>
2024-02-27 19:12:30 +0700

    add google analyst tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import DisplayModelContext from "@/components/context/model-context";
+import ThemeContext from "@/components/context/theme-context";
 import { LIST_FURNITURE } from "@/data/product_data";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
@@ -9,11 +10,32 @@ export default function App({
   pageProps: { session, ...pageProps },
 }) {
   const [displayModel, setDisplayModel] = useState(LIST_FURNITURE[0]);
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <SessionProvider session={session}>
-      <DisplayModelContext.Provider value={{ displayModel, setDisplayModel }}>
-        <Component {...pageProps} />
-      </DisplayModelContext.Provider>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9VQ6L866ZV"
+        ></script>
+        <script>
+          dangerouslySetInnerHTML=
+          {{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date()); 
+              gtag('config', 'G-9VQ6L866ZV');
+            `,
+          }}
+        </script>
+      </Head>
+      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+        <DisplayModelContext.Provider value={{ displayModel, setDisplayModel }}>
+          <Component {...pageProps} />
+        </DisplayModelContext.Provider>
+      </ThemeContext.Provider>
     </SessionProvider>
   );
 }
