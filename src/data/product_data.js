8e1fc6fb5b76ba3879591a57ce4236f48b56a062
namke-dev <namke.dev@gmail.com>
2024-03-01 13:22:04 +0700

    update product data

--- src/data/product_data.js
+++ src/data/product_data.js
@@ -53,6 +53,23 @@ export const PRODUCT_DATA = [
     
     Easily moved and locked in place for stability in your home. Both wheels are equipped with brakes, which are convenient for moving and folding`,
   },
+  {
+    Name: "Modern office desk",
+    Brand: "IDEALHOUSE",
+    Size: `11"D x 12"W x 7"H`,
+    Material: "Polyester, Cotton",
+    "Recommended Uses": "Office room",
+    Rating: 4.7,
+    "Special Feature": "Washable",
+    Price: "$139.99",
+    About: `Ideal for small apartments and limited spaces. Our drop leaf table would fully folded and stored in a small corver when not in use, maximizing your space.
+
+    Perfect as the folding kitchen table and folding dinner table!
+    
+    Just folding one tabletop, it could as an office desk placed your computer, books, camera on the tabletop！
+    
+    Easily moved and locked in place for stability in your home. Both wheels are equipped with brakes, which are convenient for moving and folding`,
+  },
 ];
 
 export const LIST_FURNITURE = [
