4280c34cd39f9e612a89d56c36be8c2033a57d67
namke-dev <namke.dev@gmail.com>
2024-03-13 22:23:40 +0700

    test account manage control

--- src/components/layouts/HamburgerMenu.jsx
+++ src/components/layouts/HamburgerMenu.jsx
@@ -1,10 +1,27 @@
+"use client";
 import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { FaAngleRight, FaHome, FaUser, FaFileAlt } from "react-icons/fa";
+import { UserAuth } from "../context/auth-context";
+import { USER_PROFILE_DATA } from "@/data/user_profile_data";
 
 const HamburgerMenu = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const sidebarRef = useRef(null);
+  const { user } = UserAuth();
+  const [userRole, setUserRole] = useState("");
+
+  useEffect(() => {
+    if (user) {
+      const userEmail = user.email;
+      const userProfile = USER_PROFILE_DATA.find(
+        (user) => user.email === userEmail
+      );
+      setUserRole(userProfile?.role || "");
+      console.log(`==> ${user}`);
+      console.log(`==> ${userRole}`);
+    }
+  }, [user]);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -29,24 +46,25 @@ const HamburgerMenu = () => {
     };
   }, [menuOpen]);
 
-  return (
-    <div className="fixed top-0 left-0 z-40">
-      <button
-        className={`focus:outline-none flex flex-col justify-around h-6 w-6 
+  if (userRole === "admin") {
+    return (
+      <div className="fixed top-0 left-0 z-40">
+        <button
+          className={`focus:outline-none flex flex-col justify-around h-6 w-6 
         mt-7 ml-5
         transform ${
           menuOpen ? "translate-x-full" : "translate-x-0"
         } transition-transform ease-in-out duration-300`}
-        onClick={toggleMenu}
-      >
-        <div className="hamburger-line bg-black dark:bg-white/70 rounded-xl"></div>
-        <div className="hamburger-line bg-black dark:bg-white/70 rounded-xl"></div>
-        <div className="hamburger-line bg-black dark:bg-white/70 rounded-xl"></div>
-      </button>
+          onClick={toggleMenu}
+        >
+          <div className="hamburger-line bg-black dark:bg-white/70 rounded-xl"></div>
+          <div className="hamburger-line bg-black dark:bg-white/70 rounded-xl"></div>
+          <div className="hamburger-line bg-black dark:bg-white/70 rounded-xl"></div>
+        </button>
 
-      <div
-        ref={sidebarRef}
-        className={`menu fixed top-0 left-0 w-3/4 md:w-[300px] bg-white/90 dark:bg-neutral-800/90 
+        <div
+          ref={sidebarRef}
+          className={`menu fixed top-0 left-0 w-3/4 md:w-[300px] bg-white/90 dark:bg-neutral-800/90 
         border-r border-gray-300  dark:border-neutral-600
         p-4 shadow h-full transform ${
           menuOpen ? "translate-x-0" : "-translate-x-full"
@@ -54,32 +72,33 @@ const HamburgerMenu = () => {
         pt-20
         flex flex-col gap-5
         z-10`}
-      >
-        <div
-          className="px-1 py-4 text-lg 
-          font-bold text-amber-600 text-center"
         >
-          MANAGEMENT MENU
+          <div
+            className="px-1 py-4 text-lg 
+          font-bold text-amber-600 text-center"
+          >
+            MANAGEMENT MENU
+          </div>
+          <SidebarButton link="/dashboard" icon={<FaHome />}>
+            DASHBOARD
+          </SidebarButton>
+          <SidebarButton link="/account-manager" icon={<FaUser />}>
+            ACCOUNT MANAGER
+          </SidebarButton>
+          <SidebarButton link="/posts-manager" icon={<FaFileAlt />}>
+            POST MANAGER
+          </SidebarButton>
         </div>
-        <SidebarButton link="/dashboard" icon={<FaHome />}>
-          DASHBOARD
-        </SidebarButton>
-        <SidebarButton link="/account-manager" icon={<FaUser />}>
-          ACCOUNT MANAGER
-        </SidebarButton>
-        <SidebarButton link="/posts-manager" icon={<FaFileAlt />}>
-          POST MANAGER
-        </SidebarButton>
-      </div>
 
-      <style jsx>{`
-        .hamburger-line {
-          width: 1.7rem;
-          height: 0.2rem;
-        }
-      `}</style>
-    </div>
-  );
+        <style jsx>{`
+          .hamburger-line {
+            width: 1.7rem;
+            height: 0.2rem;
+          }
+        `}</style>
+      </div>
+    );
+  } else return "";
 };
 
 export default HamburgerMenu;
