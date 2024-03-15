7fa6c649fe69e146645471530ce609db069d5fd3
namke-dev <namke.dev@gmail.com>
2024-03-15 23:27:48 +0700

    add styling footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="dark:bg-white/10 bg-black/60">
+    <footer className="dark:bg-white/10 bg-gray-200">
       <div
         className="flex flex-col px-5 md:px-14 py-8 
-         text-white/70 text-xs md:text-sm gap-0
+         text-black/70 text-xs md:text-sm gap-0
         w-full max-w-[1400px] m-auto"
       >
         <p className="font-semibold pb-2 text-amber-500">
